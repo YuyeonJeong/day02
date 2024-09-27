832942a1bf55fa768cfcf17345d8280707ec4d4b
user <user@email.com>
2024-09-27 16:51:40 +0900

    day02

---
+++ src/otherquiz/Quiz01_02.java
@@ -0,0 +1,13 @@
+package otherquiz;
+
+import java.util.Scanner;
+
+public class Quiz01_02 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("메뉴입니다");
+		
+		
+	}
+
+}
