800fe01dd5350f6ef8ce02c764111e26c0d5491c
user <user@email.com>
2024-09-27 16:28:59 +0900

    day02

---
+++ src/myquiz/QuizQuestion.java
@@ -0,0 +1,14 @@
+package myquiz;
+
+public class QuizQuestion {
+	public static void main(String[] args) {
+		System.out.println("=== 피자가게 문제 ===");
+		System.out.println("1. 예약하셨습니까? yes 또는 no 입력해주세요. 를 출력하세요");
+		System.out.println("2. if-else문을 활용해서 yes 또는 no 입력받고 입력값이 yes면 예약확인 되었습니다. no면 대기 시간이 발생할 수 있습니다 를 출력하세요");
+		System.out.println("3. 주문하실 메뉴를 선택해주세요. 를 출력세요");
+		System.out.println("4. 1. 페퍼로니 피자 2. 고구마 피자 3. 하와이안 피자 를 출력하세요");
+		System.out.println("5. if 문을 활용해서 1을 입력 받으면 페퍼로니 피자의 가격은 17000원입니다. 2를 입력 받으면 고구마 피자의 가격은 16000원입니다. 3을 입력 받으면 하와이안 피자의 가격은 15000원입니다. 를 각각 출력하세요");
+		System.out.println("6. 매장을 이용할 인원수 입력받고 삼항 연산자를 활용해서 그 값이 3의 배수이면 축하합니다! 3월 맞이 3배수 이벤트에 당첨되어 음료 무료쿠폰이 발급되었습니다. 를 출력하고 3의 배수가 아니면 -명 입력되었습니다. 를 출력하세요");
+	}
+
+}
